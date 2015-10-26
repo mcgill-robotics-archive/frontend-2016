e54b1222eacf990b049beb6a8db907b1acc6d886
Anass Al <mr@anassinator.com>
2015-10-26 18:17:17 -0400

    Add bare rosrunnable npm package

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env nodejs
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello McGill Robotics!');
+})
+
+var server = app.listen(3000, '0.0.0.0', function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Running on http://%s:%s', host, port);
+});
