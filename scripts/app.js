1d4a440dd7da8a9875ca0b2839093a03eeac8183
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2015-10-29 23:20:15 -0400

    Add missing semicolon

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello McGill Robotics!');
-})
+});
 
 var server = app.listen(3000, '0.0.0.0', function() {
   var host = server.address().address;
