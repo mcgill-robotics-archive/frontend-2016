c93c85eae032df7301105c9e6bf1634d48db1f49
David Lougheed <david.lougheed@gmail.com>
2015-11-04 11:15:40 -0500

    Add frontend interface utility object constructor and move common code into it

---
+++ public/scripts/interface.js
@@ -0,0 +1,28 @@
+/*global ROSLIB */
+
+'use strict';
+
+/**
+ * Defines a globally accessible object with commonly used javascript
+ * parameters, functions, and other resources.
+ * @author David Lougheed
+ * @global 
+ */
+var MRFrontendInterface = function() {
+  this.ros = new ROSLIB.Ros({
+    url: 'ws://localhost:8080'
+  });
+  
+  this.ros.on('connection', function () {
+    console.log('Connected to websocket server.');
+  });
+  this.ros.on('error', function (error) {
+    console.log('Error connecting to websocket server: ', error);
+  });
+
+  this.polymerBaseProperties = {
+    topic: String,
+    messageType: String
+  }
+}
+
