4784335a96c5e7bba40416dad28d4cb4012e3598
David Lougheed <david.lougheed@gmail.com>
2015-11-03 20:20:40 -0500

    Fix indents in ROS client setup script and remove unneeded uses of context variable in text component template

--- public/scripts/ros-setup.js
+++ public/scripts/ros-setup.js
@@ -1,13 +1,16 @@
+/*global ROSLIB */
 
-  window.ros = new ROSLIB.Ros({
-    url: 'ws://localhost:8080'
-  });
+'use strict';
 
-  window.ros.on('connection', function () {
-    console.log('Connected to websocket server.');
-  });
+window.ros = new ROSLIB.Ros({
+  url: 'ws://localhost:8080'
+});
 
-  window.ros.on('error', function (error) {
-    console.log('Error connecting to websocket server: ', error);
-  });
+window.ros.on('connection', function () {
+  console.log('Connected to websocket server.');
+});
+
+window.ros.on('error', function (error) {
+  console.log('Error connecting to websocket server: ', error);
+});
 
