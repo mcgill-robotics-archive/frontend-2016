008fd6426e0242a478a85f2abb096894e401ac0d
Yichi Zhang <yichi.zhang@mail.mcgill.ca>
2015-11-17 15:09:37 -0500

    Committed test file that would be deleted. Check out /views/index.jade. The base-widget.html is the local file and base-widget.js is the javascript that runs the html.

---
+++ public/elements/base-widget.js
@@ -0,0 +1,16 @@
+/**
+ * @file Defines behaviours for base widget cards.
+ */
+
+/*global Polymer, ROSLIB, frontendInterface */
+
+'use strict';
+
+Polymer({
+  is: "base-widget",
+
+    /*
+     * Build an object with all Polymer properties 
+     * for the base widget. Testing file
+     */
+});
