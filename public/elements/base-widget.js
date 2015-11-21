1dca98717aeab37933a320fb1a6f0372aa73b3bb
David Lougheed <david.lougheed@gmail.com>
2015-11-21 14:01:32 -0500

    Fix non-functional Polymer base-widget element

--- public/elements/base-widget.js
+++ public/elements/base-widget.js
@@ -7,10 +7,11 @@
 'use strict';
 
 Polymer({
-  is: "base-widget",
+  is: "base-widget"
 
     /*
      * Build an object with all Polymer properties 
      * for the base widget. Testing file
      */
 });
+
