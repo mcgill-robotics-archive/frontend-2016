82f380168d08dd76b75d2d955f3954d67f14b212
David Lougheed <david.lougheed@gmail.com>
2016-01-19 22:19:05 -0500

    Remove unneeded code and comments

--- public/elements/base-widget/base-widget.js
+++ public/elements/base-widget/base-widget.js
@@ -9,11 +9,6 @@
 var BaseWidget = Polymer({
   is: 'base-widget',
 
-    /*
-     * Build an object with all Polymer properties 
-     * for the base widget. Testing file
-     */
-
   properties: {
     title: String,
 
@@ -29,36 +24,6 @@ var BaseWidget = Polymer({
     this.position = position;
     this.components = [];
 
-    this.addComponent(new BarComponent({
-      'topic': '/test_text_topic',
-      'message-type': 'std_msgs/Int64',
-
-      'label': 'Number',
-
-      'min': 0,
-      'max': 1000,
-      'step': 1
-    }));
-  },
-
-  refreshDOM: function () {
-    var container = this.getElementsByClassName('componentContainer')[0],
-      c;
-    container.innerHTML = '';
-
-    for (c in this.components) {
-      if (this.components.hasOwnProperty(c)) {
-        container.appendChild(this.components[c]);
-      }
-    }
-  },
-
-  addComponent: function (component) {
-    this.components.push(component);
-    this.refreshDOM();
-  },
-
-  setUpComponent: function (e) {
     this.addComponent(new BarComponent({
       'topic': '/test_text_topic',
       'message-type': 'std_msgs/Int64',
