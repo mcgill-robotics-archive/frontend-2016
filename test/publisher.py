5efbedab9741ca802276576420eebe0aca438f9f
Malcolm Watt <malcolm.watt@mail.mcgill.ca>
2016-01-21 18:58:11 -0500

    Test pose publisher

--- test/publisher.py
+++ test/publisher.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
+import random
 from std_msgs.msg import Int64
+from std_msgs.msg import Pose
+from std_msgs.msg import Quaternion
+from std_msgs.msg import Point
+from std_msgs.msg import PoseStamped
+
+def createPoseMessage():
+  angle = random.random() * 2 * math.pi
+  quat = Quaternion(angle, 0, 0, 1)
+  return PoseStamped(Header(), Pose(Point(0, 0, 0), Quaternion(quat)))
 
 def testPublish():
   rospy.init_node("frontend_test_publisher")
 
   # Create publisher for test text topic
   pub = rospy.Publisher("/test_text_topic", Int64, queue_size=1)
+  posePub = rospy.Publisher("/test_pose_topic", PoseStamped, queue_size=1)
 
   # Set test message rate
   rate = rospy.Rate(1)
@@ -16,6 +27,9 @@ def testPublish():
 
   # Run until stopped.
   while not rospy.is_shutdown():
+    posemsg = createPoseMessage()
+    posePub.publish(posemsg)
+
     msg = Int64(count)
     count += 1
     pub.publish(msg)
