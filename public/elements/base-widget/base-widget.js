6b5d96f8b89c4a42aad78c4285f37a711863ea8c
David Lougheed <david.lougheed@gmail.com>
2016-01-20 22:10:11 -0500

    Fix spacing and alignment and document column functionality in base widget

--- public/elements/base-widget/base-widget.js
+++ public/elements/base-widget/base-widget.js
@@ -23,14 +23,22 @@ var BaseWidget = Polymer({
    * @function
    */
   attached: function () {
+    /*
+     * Divide 12-column size parameter into a decimal percentage, then
+     * multiply by 100 to convert to CSS percentages.
+     */
     var container = document.getElementById('interfaceContainer');
-    this.customStyle['--widget-width'] = (this.width / 12.0 * 100).toString()
-      + '%';
-    this.customStyle['--widget-height'] = (this.height / 12.0
+    this.customStyle['--widget-width'] = 
+      (this.width / 12.0 * 100).toString() + '%';
+
+    this.customStyle['--widget-height'] = (this.height / 12.0 
       * container.clientHeight).toString() + 'px';
+
     this.customStyle['--widget-left'] = (this.x / 12.0 * 100).toString() + '%';
+
     this.customStyle['--widget-top'] = (this.y / 12.0
       * container.clientHeight).toString() + 'px';
+
     this.updateStyles();
   }
 });
