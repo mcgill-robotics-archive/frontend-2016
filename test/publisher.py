8e34107120482906f2e231ffa292f5925210ad5a
Tim Flichy <timothee.flichy@mail.mcgill.ca>
2016-02-01 12:58:15 -0500

    Fix indent

--- test/publisher.py
+++ test/publisher.py
@@ -4,27 +4,27 @@ import rospy
 from std_msgs.msg import Int64
 
 def testPublish():
-  rospy.init_node("frontend_test_publisher")
+    rospy.init_node("frontend_test_publisher")
 
-  # Create publisher for test text topic
-  pub = rospy.Publisher("/test_text_topic", Int64, queue_size=1)
+    # Create publisher for test text topic
+    pub = rospy.Publisher("/test_text_topic", Int64, queue_size=1)
 
-  # Set test message rate
-  rate = rospy.Rate(1)
+    # Set test message rate
+    rate = rospy.Rate(1)
 
-  count = 0
+    count = 0
 
-  # Run until stopped.
-  while not rospy.is_shutdown():
-    msg = Int64(count)
-    count += 1
-    pub.publish(msg)
+    # Run until stopped.
+    while not rospy.is_shutdown():
+        msg = Int64(count)
+        count += 1
+        pub.publish(msg)
 
     rate.sleep()
 
 if __name__ == "__main__":
-  try:
-    testPublish()
-  except rospy.ROSInterruptException:
-    pass
+    try:
+        testPublish()
+    except rospy.ROSInterruptException:
+        pass
 
